0ad921cabf9c639df6a8c5402758e5b50b378608
timelessnesses <mooping3roblox@gmail.com>
2023-02-19 11:28:01 +0700

    improved

--- util.py
+++ util.py
@@ -2,4 +2,6 @@ import os
 
 linuxes = [x for x in os.listdir('dist/') if 'linux' in x]
 
-os.system('auditwheel repair dist/%s -w dist/ --plat x86_64' % linuxes[0])
+for linux in linuxes:
+	os.system('auditwheel repair dist/%s -w dist/ --plat manylinux_2_5_x86_64' % linux)
+	os.remove('dist/'+linux)
