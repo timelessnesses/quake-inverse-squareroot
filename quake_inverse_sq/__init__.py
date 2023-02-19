023e9f8aa16960f42a6cac899f4cc1fcfe6f87e6
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2023-02-19 21:29:13 +0700

    Update __init__.py

--- quake_inverse_sq/__init__.py
+++ quake_inverse_sq/__init__.py
@@ -1,9 +1,28 @@
 """
 Inverse square root implementations. (Implementation from Wikipedia and math.h)
 """
+import logging
+logger = logging.getLogger('quake_inverse_sq')
+try:
+    from ._sqrt import coarse_inv_sqrt as __c
+    from ._sqrt import fined_inv_sqrt as __f
+except ImportError:
+    from ctypes import c_float, c_int32, cast, byref, POINTER
+    from math import sqrt
+    def __c(number: float):
+        THREEHALFS = 1.5
+        x2 = number * 0.5
+        y = c_float(number)
+        WHAT_THE_FUCK = 0x5f3759df
 
-from ._sqrt import coarse_inv_sqrt as c
-from ._sqrt import fined_inv_sqrt as f
+        i = cast(byref(y), POINTER(c_int32)).contents.value
+        i = c_int32(WHAT_THE_FUCK - (i >> 1))
+        y = cast(byref(i), POINTER(c_float)).contents.value
+
+        y = y * (THREEHALFS - (x2 * y * y))
+        return y
+    def __f(number: float):
+        return 1/sqrt(x)
 
 def coarse_inv_sqrt(number:float) -> float:
     """
@@ -13,7 +32,7 @@ def coarse_inv_sqrt(number:float) -> float:
         raise ZeroDivisionError
     elif not isinstance(number, (float,int)):
         raise ValueError("Inappropriate argument value")
-    return c(number)
+    return __c(number)
 
 def fined_inv_sqrt(number: float) -> float:
     """
@@ -23,4 +42,5 @@ def fined_inv_sqrt(number: float) -> float:
         raise ZeroDivisionError
     elif not isinstance(number, (float,int)):
         raise ValueError("Inappropriate argument value")
-    return f(number)
+    return __f(number)
+
